4860c91fa1e843502facd4fc1873d04602bafa57
Selitskiy <77249557+Selitskiy@users.noreply.github.com>
2021-04-13 12:02:13 -0400

    Add files via upload

--- pred_dist2.R
+++ pred_dist2.R
@@ -20,7 +20,7 @@ probs1 <- c(.75,.9,.95)
 probs2 <- c(.25,.1,.05)
 
 
-fname <- "predict_an_6b_nm.txt"
+fname <- "predict_ires2_6b_nm.txt"
 nm_test <- read.delim(fname, sep = "", header = F, na.strings = " ", fill = T)
 nr <- nrow(nm_test)
 for (i in 1:nr){
@@ -40,19 +40,19 @@ for (i in 1:nr){
 tr <- t1[t1$Flag == TRUE,]
 tw <- t1[t1$Flag != TRUE,]
 
-tr1 <- t1[t1$Flag == TRUE & t1$Correct_class == "S1",]
-tw1 <- t1[t1$Flag != TRUE & t1$Correct_class == "S1",]
-tr2 <- t1[t1$Flag == TRUE & t1$Correct_class == "S2",]
-tw2 <- t1[t1$Flag != TRUE & t1$Correct_class == "S2",]
-tr3 <- t1[t1$Flag == TRUE & t1$Correct_class == "S3",]
-tw3 <- t1[t1$Flag != TRUE & t1$Correct_class == "S3",]
-tr4 <- t1[t1$Flag == TRUE & t1$Correct_class == "S4",]
-tw4 <- t1[t1$Flag != TRUE & t1$Correct_class == "S4",]
-tr5 <- t1[t1$Flag == TRUE & t1$Correct_class == "S5",]
-tw5 <- t1[t1$Flag != TRUE & t1$Correct_class == "S5",]
+#tr1 <- t1[t1$Flag == TRUE & t1$Correct_class == "S1",]
+#tw1 <- t1[t1$Flag != TRUE & t1$Correct_class == "S1",]
+#tr2 <- t1[t1$Flag == TRUE & t1$Correct_class == "S2",]
+#tw2 <- t1[t1$Flag != TRUE & t1$Correct_class == "S2",]
+#tr3 <- t1[t1$Flag == TRUE & t1$Correct_class == "S3",]
+#tw3 <- t1[t1$Flag != TRUE & t1$Correct_class == "S3",]
+#tr4 <- t1[t1$Flag == TRUE & t1$Correct_class == "S4",]
+#tw4 <- t1[t1$Flag != TRUE & t1$Correct_class == "S4",]
+#tr5 <- t1[t1$Flag == TRUE & t1$Correct_class == "S5",]
+#tw5 <- t1[t1$Flag != TRUE & t1$Correct_class == "S5",]
 
 
-fname <- "predict_an_6b_m.txt"
+fname <- "predict_ires2_6b_m.txt"
 m_test <- read.delim(fname, sep = "", header = T, na.strings = " ", fill = T)
 mr <- nrow(m_test)
 for (i in 1:mr){
@@ -138,6 +138,7 @@ Dnm
 ks.test(t2wr, twr)
 
 
+#Prior
 #Untrusted Accuracy
 sum(tr$P1)/(length(tr$P1) + length(tw$P1))
 
@@ -165,6 +166,8 @@ sum(tw$P1 <= thr[3])/length(tw$P1)
 
 
 
+
+#Posterior
 #Untrusted Accuracy
 sum(t2r$P1)/(length(t2r$P1) + length(t2w$P1))
 
@@ -181,7 +184,7 @@ sum(t2r$P1 > thrp[1])/length(t2r$P1)
 sum(t2r$P1 > thrp[2])/length(t2r$P1)
 sum(t2r$P1 > thrp[3])/length(t2r$P1)
 
-#Percentile
+#Percentile verification
 sum(t2w$P1 <= thrp[1])/length(t2w$P1)
 sum(t2w$P1 <= thrp[2])/length(t2w$P1)
 sum(t2w$P1 <= thrp[3])/length(t2w$P1)
@@ -204,7 +207,7 @@ sum(t2r$P1 > thr[1])/length(t2r$P1)
 sum(t2r$P1 > thr[2])/length(t2r$P1)
 sum(t2r$P1 > thr[3])/length(t2r$P1)
 
-#Percentile
+#Percentile verification
 sum(t2w$P1 <= thr[1])/length(t2w$P1)
 sum(t2w$P1 <= thr[2])/length(t2w$P1)
 sum(t2w$P1 <= thr[3])/length(t2w$P1)
@@ -212,188 +215,188 @@ sum(t2w$P1 <= thr[3])/length(t2w$P1)
 
 
 #Trustd Accuracy
-sum(t2r$P2 < thr2[1] & t2r$P1 > thr[1])/(length(t2r$P2) + length(t2w$P2))
-sum(t2r$P2 < thr2[2] & t2r$P1 > thr[2])/(length(t2r$P2) + length(t2w$P2))
-sum(t2r$P2 < thr2[3] & t2r$P1 > thr[3])/(length(t2r$P2) + length(t2w$P2))
+#sum(t2r$P2 < thr2[1] & t2r$P1 > thr[1])/(length(t2r$P2) + length(t2w$P2))
+#sum(t2r$P2 < thr2[2] & t2r$P1 > thr[2])/(length(t2r$P2) + length(t2w$P2))
+#sum(t2r$P2 < thr2[3] & t2r$P1 > thr[3])/(length(t2r$P2) + length(t2w$P2))
 #Precision
-sum(t2r$P2 < thr2[1] & t2r$P1 > thr[1])/(sum(t2r$P2 < thr2[1] & t2r$P1 > thr[1]) + sum(t2w$P2 < thr2[1] & t2w$P1 > thr[1]))
-sum(t2r$P2 < thr2[2] & t2r$P1 > thr[2])/(sum(t2r$P2 < thr2[2] & t2r$P1 > thr[2]) + sum(t2w$P2 < thr2[2] & t2w$P1 > thr[2]))
-sum(t2r$P2 < thr2[3] & t2r$P1 > thr[3])/(sum(t2r$P2 < thr2[3] & t2r$P1 > thr[3]) + sum(t2w$P2 < thr2[3] & t2w$P1 > thr[3]))
+#sum(t2r$P2 < thr2[1] & t2r$P1 > thr[1])/(sum(t2r$P2 < thr2[1] & t2r$P1 > thr[1]) + sum(t2w$P2 < thr2[1] & t2w$P1 > thr[1]))
+#sum(t2r$P2 < thr2[2] & t2r$P1 > thr[2])/(sum(t2r$P2 < thr2[2] & t2r$P1 > thr[2]) + sum(t2w$P2 < thr2[2] & t2w$P1 > thr[2]))
+#sum(t2r$P2 < thr2[3] & t2r$P1 > thr[3])/(sum(t2r$P2 < thr2[3] & t2r$P1 > thr[3]) + sum(t2w$P2 < thr2[3] & t2w$P1 > thr[3]))
 
 
 
-ggplot(tr, aes(x=P2)) + 
-    #geom_histogram(data=tw, fill="green", binwidth = 0.01, alpha=0.6) +
-    geom_histogram(data=tw1, fill="red1", binwidth = 0.01, alpha=0.6) +
-    geom_histogram(data=tw2, fill="red2", binwidth = 0.01, alpha=0.6) +
-    geom_histogram(data=tw3, fill="red3", binwidth = 0.01, alpha=0.6) +
-    geom_histogram(data=tw4, fill="red4", binwidth = 0.01, alpha=0.6) +
-    geom_histogram(data=tw5, fill="red", binwidth = 0.01, alpha=0.6) +
-  geom_histogram(data=tr1, fill="green1", binwidth = 0.01, alpha=0.6) +
-  geom_histogram(data=tr2, fill="green2", binwidth = 0.01, alpha=0.6) +
-  geom_histogram(data=tr3, fill="green3", binwidth = 0.01, alpha=0.6) +
-  geom_histogram(data=tr4, fill="green4", binwidth = 0.01, alpha=0.6) +
-  geom_histogram(data=tr5, fill="green", binwidth = 0.01, alpha=0.6) +
-  #geom_density(data=tw, color="red", fill="red", adjust=0.25, alpha=0.6) +
-  #geom_density(data=tr, color="green", fill="green", adjust=0.25, alpha=0.6) +
-  labs(title="Correctly (green) and wrongly (red) recognized image distributions (no-makeup training)",
-       x="Softmax activations (probability/score)", y="Density") +
-  xlim(0, .55) + ylim(0, 25)
+#ggplot(tr, aes(x=P2)) + 
+#    #geom_histogram(data=tw, fill="green", binwidth = 0.01, alpha=0.6) +
+#    geom_histogram(data=tw1, fill="red1", binwidth = 0.01, alpha=0.6) +
+#    geom_histogram(data=tw2, fill="red2", binwidth = 0.01, alpha=0.6) +
+#    geom_histogram(data=tw3, fill="red3", binwidth = 0.01, alpha=0.6) +
+#    geom_histogram(data=tw4, fill="red4", binwidth = 0.01, alpha=0.6) +
+#    geom_histogram(data=tw5, fill="red", binwidth = 0.01, alpha=0.6) +
+#  geom_histogram(data=tr1, fill="green1", binwidth = 0.01, alpha=0.6) +
+#  geom_histogram(data=tr2, fill="green2", binwidth = 0.01, alpha=0.6) +
+#  geom_histogram(data=tr3, fill="green3", binwidth = 0.01, alpha=0.6) +
+#  geom_histogram(data=tr4, fill="green4", binwidth = 0.01, alpha=0.6) +
+#  geom_histogram(data=tr5, fill="green", binwidth = 0.01, alpha=0.6) +
+#  #geom_density(data=tw, color="red", fill="red", adjust=0.25, alpha=0.6) +
+#  #geom_density(data=tr, color="green", fill="green", adjust=0.25, alpha=0.6) +
+#  labs(title="Correctly (green) and wrongly (red) recognized image distributions (no-makeup training)",
+#       x="Softmax activations (probability/score)", y="Density") +
+#  xlim(0, .55) + ylim(0, 25)
 
-describe(tw$P2)
-describe(tr$P2)
+#describe(tw$P2)
+#describe(tr$P2)
 
-describe(tw1$P2)
-describe(tw2$P2)
-describe(tw3$P2)
+#describe(tw1$P2)
+#describe(tw2$P2)
+#describe(tw3$P2)
 
-describe(tr1$P2)
-describe(tr2$P2)
-describe(tr3$P2)
-
-
-
-ggplot(tr, aes(x=P2)) + 
-  geom_histogram(data=tr, fill="green", binwidth = 0.01, alpha=0.6) +
-  geom_histogram(data=tw, fill="red", binwidth = 0.01, alpha=0.6) +
-  #geom_density(data=tw, color="red", fill="red", adjust=0.25, alpha=0.6) +
-  #geom_density(data=tr, color="green", fill="green", adjust=0.25, alpha=0.6) +
-  labs(title="Correctly (green) and wrongly (red) recognized image distributions (no-makeup training)",
-       x="Softmax activations (probability/score)", y="Density") +
-  xlim(0, 1.05) + ylim(0, 70)
-
-describe(tr$P2)
-describe(tw$P2)
+#describe(tr1$P2)
+#describe(tr2$P2)
+#describe(tr3$P2)
 
 
+
+#ggplot(tr, aes(x=P2)) + 
+#  geom_histogram(data=tr, fill="green", binwidth = 0.01, alpha=0.6) +
+#  geom_histogram(data=tw, fill="red", binwidth = 0.01, alpha=0.6) +
+#  #geom_density(data=tw, color="red", fill="red", adjust=0.25, alpha=0.6) +
+#  #geom_density(data=tr, color="green", fill="green", adjust=0.25, alpha=0.6) +
+#  labs(title="Correctly (green) and wrongly (red) recognized image distributions (no-makeup training)",
+#       x="Softmax activations (probability/score)", y="Density") +
+#  xlim(0, 1.05) + ylim(0, 70)
+
+#describe(tr$P2)
+#describe(tw$P2)
 
 
 
 
 
-t1 <- acc[1:nr,]
-t1[5] <- FALSE
-t1[6] <- FALSE
-t1[7] <- FALSE
-t1[8] <- FALSE
-#rep(FALSE, nr)
-
-names(t1) <- c("Correct_class", "Score", "Guess_class","File", "Flag", "Flag1", "Flag2", "Flag3")
-
-for (i in 1:nr){
-  if( t1$Correct_class[i] == t1$Guess_class[i] ){
-    t1$Flag[i] <- TRUE
-  }
-}
-
-tr <- t1[t1$Flag == TRUE,]
-tw <- t1[t1$Flag != TRUE,]
-
-data <- as.data.frame(t1)
-
-#ggplot(data, aes(x=GuessScore, color=V26, fill=V26)) + 
-#  geom_histogram(binwidth = 0.05, alpha=0.6)
-
-ggplot(tr, aes(x=Score)) + 
-#  geom_histogram(data=tr, fill="green", binwidth = 0.02, alpha=0.6) +
-#  geom_histogram(data=tw, fill="red", binwidth = 0.02, alpha=0.6) +
-  geom_density(data=tw, color="red", fill="red", adjust=0.25, alpha=0.6) +
-  geom_density(data=tr, color="green", fill="green", adjust=0.25, alpha=0.6) +
-  labs(title="Correctly (green) and wrongly (red) recognized image distributions (no-makeup training)",
-       x="Softmax activations (probability/score)", y="Density") +
-  xlim(0, 1.05) + ylim(0, 175)
-
-describe(tr$Score)
-
-describe(tw$Score)
-
-thr <- quantile(tw$Score, probs)
-
-thr
-
-#tr$Flag1[tr$Score >= thr[1]] <- TRUE
-#tr$Flag2[tr$Score >= thr[2]] <- TRUE
-#tr$Flag3[tr$Score >= thr[3]] <- TRUE
-
-sum(tr$Score > thr[1])/(length(tr$Score) + length(tw$Score))
-sum(tr$Score > thr[2])/(length(tr$Score) + length(tw$Score))
-sum(tr$Score > thr[3])/(length(tr$Score) + length(tw$Score))
 
 
+#t1 <- acc[1:nr,]
+#t1[5] <- FALSE
+#t1[6] <- FALSE
+#t1[7] <- FALSE
+#t1[8] <- FALSE
+##rep(FALSE, nr)
 
-fname2 <- "predict_5d2.txt"
+#names(t1) <- c("Correct_class", "Score", "Guess_class","File", "Flag", "Flag1", "Flag2", "Flag3")
+
+#for (i in 1:nr){
+#  if( t1$Correct_class[i] == t1$Guess_class[i] ){
+#    t1$Flag[i] <- TRUE
+#  }
+#}
+
+#tr <- t1[t1$Flag == TRUE,]
+#tw <- t1[t1$Flag != TRUE,]
+
+#data <- as.data.frame(t1)
+
+##ggplot(data, aes(x=GuessScore, color=V26, fill=V26)) + 
+##  geom_histogram(binwidth = 0.05, alpha=0.6)
+
+#ggplot(tr, aes(x=Score)) + 
+##  geom_histogram(data=tr, fill="green", binwidth = 0.02, alpha=0.6) +
+##  geom_histogram(data=tw, fill="red", binwidth = 0.02, alpha=0.6) +
+#  geom_density(data=tw, color="red", fill="red", adjust=0.25, alpha=0.6) +
+#  geom_density(data=tr, color="green", fill="green", adjust=0.25, alpha=0.6) +
+#  labs(title="Correctly (green) and wrongly (red) recognized image distributions (no-makeup training)",
+#       x="Softmax activations (probability/score)", y="Density") +
+#  xlim(0, 1.05) + ylim(0, 175)
+
+#describe(tr$Score)
+
+#describe(tw$Score)
+
+#thr <- quantile(tw$Score, probs)
+
+#thr
+
+##tr$Flag1[tr$Score >= thr[1]] <- TRUE
+##tr$Flag2[tr$Score >= thr[2]] <- TRUE
+##tr$Flag3[tr$Score >= thr[3]] <- TRUE
+
+#sum(tr$Score > thr[1])/(length(tr$Score) + length(tw$Score))
+#sum(tr$Score > thr[2])/(length(tr$Score) + length(tw$Score))
+#sum(tr$Score > thr[3])/(length(tr$Score) + length(tw$Score))
 
 
-acc2 <- read.delim(fname2, sep = "", header = F, na.strings = " ", fill = T)
 
-nr2 <- nrow(acc2)
-t2 <- acc2[1:nr2,]
-t2[5] <- rep(FALSE, nr2)
+#fname2 <- "predict_5d2.txt"
 
-names(t2) <- c("Correct_class", "Score", "Guess_class","File", "Flag")
 
-for (i in 1:nr2){
-  if( t2$Correct_class[i] == t2$Guess_class[i] ){
-    t2$Flag[i] <- TRUE
-  }
-}
+#acc2 <- read.delim(fname2, sep = "", header = F, na.strings = " ", fill = T)
 
-tr2 <- t2[t2$Flag == TRUE,]
-tw2 <- t2[t2$Flag != TRUE,]
+#nr2 <- nrow(acc2)
+#t2 <- acc2[1:nr2,]
+#t2[5] <- rep(FALSE, nr2)
 
-data2 <- as.data.frame(t2)
+#names(t2) <- c("Correct_class", "Score", "Guess_class","File", "Flag")
 
-ggplot(tr2, aes(x=Score)) + 
-  #  geom_histogram(data=tr2, fill="green", binwidth = 0.02, alpha=0.6) +
-  #  geom_histogram(data=tw2, fill="red", binwidth = 0.02, alpha=0.6) +
-  geom_density(data=tw2, color="red", fill="red", adjust=0.25, alpha=0.6) +
-  geom_density(data=tr2, color="green", fill="green", adjust=0.25, alpha=0.6) +
-  labs(title="Correctly (green) and wrongly (red) recognized image distributions (makeup infused training)",
-       x="Softmax activations (probability/score)", y="Density") +
-  xlim(0, 1.05) + ylim(0, 175)
+#for (i in 1:nr2){
+#  if( t2$Correct_class[i] == t2$Guess_class[i] ){
+#    t2$Flag[i] <- TRUE
+#  }
+#}
 
-describe(tr2$Score)
+#tr2 <- t2[t2$Flag == TRUE,]
+#tw2 <- t2[t2$Flag != TRUE,]
 
-describe(tw2$Score)
+#data2 <- as.data.frame(t2)
 
-thr2 <- quantile(tw2$Score, probs)
+#ggplot(tr2, aes(x=Score)) + 
+#  #  geom_histogram(data=tr2, fill="green", binwidth = 0.02, alpha=0.6) +
+#  #  geom_histogram(data=tw2, fill="red", binwidth = 0.02, alpha=0.6) +
+#  geom_density(data=tw2, color="red", fill="red", adjust=0.25, alpha=0.6) +
+#  geom_density(data=tr2, color="green", fill="green", adjust=0.25, alpha=0.6) +
+#  labs(title="Correctly (green) and wrongly (red) recognized image distributions (makeup infused training)",
+#       x="Softmax activations (probability/score)", y="Density") +
+#  xlim(0, 1.05) + ylim(0, 175)
 
-thr2
+#describe(tr2$Score)
 
-sum(tr2$Score > thr2[1])/length(tr2$Score) * length(tr2$Score)/(length(tr2$Score) + length(tw2$Score))
-sum(tr2$Score > thr2[2])/length(tr2$Score) * length(tr2$Score)/(length(tr2$Score) + length(tw2$Score))
-sum(tr2$Score > thr2[3])/length(tr2$Score) * length(tr2$Score)/(length(tr2$Score) + length(tw2$Score))
+#describe(tw2$Score)
+
+#thr2 <- quantile(tw2$Score, probs)
+
+#thr2
+
+#sum(tr2$Score > thr2[1])/length(tr2$Score) * length(tr2$Score)/(length(tr2$Score) + length(tw2$Score))
+#sum(tr2$Score > thr2[2])/length(tr2$Score) * length(tr2$Score)/(length(tr2$Score) + length(tw2$Score))
+#sum(tr2$Score > thr2[3])/length(tr2$Score) * length(tr2$Score)/(length(tr2$Score) + length(tw2$Score))
 
 
 
 #Right
-ggplot(tr, aes(x=Score)) + 
-  #  geom_histogram(data=tr2, fill="green", binwidth = 0.02, alpha=0.6) +
-  #  geom_histogram(data=tw2, fill="red", binwidth = 0.02, alpha=0.6) +
-  geom_density(data=tr, color="green", fill="green", adjust=0.25, alpha=0.6) +
-  geom_density(data=tr2, color="green4", fill="green4", adjust=0.25, alpha=0.6) +
-  xlim(0.9, 1.00) + ylim(0, 50)
+#ggplot(tr, aes(x=Score)) + 
+#  #  geom_histogram(data=tr2, fill="green", binwidth = 0.02, alpha=0.6) +
+#  #  geom_histogram(data=tw2, fill="red", binwidth = 0.02, alpha=0.6) +
+#  geom_density(data=tr, color="green", fill="green", adjust=0.25, alpha=0.6) +
+#  geom_density(data=tr2, color="green4", fill="green4", adjust=0.25, alpha=0.6) +
+#  xlim(0.9, 1.00) + ylim(0, 50)
 
 
 #wrong
-ggplot(tw, aes(x=Score)) + 
-  #  geom_histogram(data=tr2, fill="green", binwidth = 0.02, alpha=0.6) +
-  #  geom_histogram(data=tw2, fill="red", binwidth = 0.02, alpha=0.6) +
-  geom_density(data=tw, color="gray", fill="gray", adjust=0.25, alpha=0.6) +
-  geom_density(data=tw2, color="gray50", fill="gray50", adjust=0.25, alpha=0.6) +
-  xlim(0.85, 1.05) 
-#+ ylim(0, 175)
+#ggplot(tw, aes(x=Score)) + 
+#  #  geom_histogram(data=tr2, fill="green", binwidth = 0.02, alpha=0.6) +
+#  #  geom_histogram(data=tw2, fill="red", binwidth = 0.02, alpha=0.6) +
+#  geom_density(data=tw, color="gray", fill="gray", adjust=0.25, alpha=0.6) +
+#  geom_density(data=tw2, color="gray50", fill="gray50", adjust=0.25, alpha=0.6) +
+#  xlim(0.85, 1.05) 
+##+ ylim(0, 175)
 
 
 #Export
-ec <- tr[,1:3]
-write.csv(ec, "correct1.csv", row.names=F)
+#ec <- tr[,1:3]
+#write.csv(ec, "correct1.csv", row.names=F)
 
-ew <- tw[,1:3]
-write.csv(ew, "wrong1.csv", row.names=F)
+#ew <- tw[,1:3]
+#write.csv(ew, "wrong1.csv", row.names=F)
 
-ec2 <- tr2[,1:3]
-write.csv(ec2, "correct2.csv", row.names=F)
+#ec2 <- tr2[,1:3]
+#write.csv(ec2, "correct2.csv", row.names=F)
 
-ew2 <- tw2[,1:3]
-write.csv(ew2, "wrong2.csv", row.names=F)
+#ew2 <- tw2[,1:3]
+#write.csv(ew2, "wrong2.csv", row.names=F)
